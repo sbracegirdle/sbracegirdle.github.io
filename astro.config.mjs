833c6d182b43a4b07a6744526265094757d57a36
Simon Bracegirdle <sbracegirdle@gmail.com>
2023-06-24 13:05:47 +0800

    Fix; optimise images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://letsbuild.cloud",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    image(),
   ],
 });
